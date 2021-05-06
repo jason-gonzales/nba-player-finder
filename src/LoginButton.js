69f6c222960a9891e839079589218bde1304c040
Jason Gonzales <jasongonzo25@gmail.com>
2021-05-05 21:39:44 -0700

    complete

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
 
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
 
